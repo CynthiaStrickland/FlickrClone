7ac5dbf58fd24096d8a0b1959e018e04e51471ed
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2016-02-19 14:56:45 -0800

    no message

--- FlickrHelper.swift
+++ FlickrHelper.swift
@@ -10,12 +10,18 @@ import UIKit
 import Foundation
 
 class FlickrHelper: NSObject {
-    class func URLForSearchString (searchString:String) ->String {
+    class func URLForSearchString (searchString:String) -> String {
+        
         let apiKey:String = "0ff53deaa3d0462576446bb28eab1edc"
-//        let search:String = searchString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet)NSUTF8StringEncoding
         
-        return "http://api.flickr.com/services/rest method=flickr.photos.search&api_key=0ff53deaa3d0462576446bb28eab1edc&format=rest&api_sig=f24f4e98063a9b8ecc8b522b238 d5e2f"
+        let search:String = searchString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.alphanumericCharacterSet())!
         
-        
+        return "https://api.flickr.com/services/rest ?method=flickr.photos.search&api_key=\(apiKey)&text=\(search)&per_page=20&format=json&nojsoncallback-1"
+    }
+    
+    class func URLForFlickrPhoto(photo:FlickrPhoto, size:String) ->String
+    var _size:String = size
+    if _size.isEmpty {
+    
     }
 }
